cbae0a07a81031a8371a9a2a65e919054ca75a00
lucas <lucasaita4000@gmail.com>
2025-09-11 19:57:14 -0300

    fix(infra): editando e configurando melhor a criacao da nossa entidade no db

--- src/main/resources/db/migration/V1__create_table_events.sql
+++ src/main/resources/db/migration/V1__create_table_events.sql
@@ -4,7 +4,7 @@ CREATE TABLE events (
     description TEXT,
     dateStart TIMESTAMP NOT NULL,
     dateFinish TIMESTAMP NOT NULL,
-    identificator VARCHAR(255) NOT NULL,
+    identificator VARCHAR(255) NOT NULL UNIQUE,
     localEvent VARCHAR(255) NOT NULL,
     organization VARCHAR(255),
     capacity VARCHAR(255),
