71a775f552e698ed00eb7a7c15cdbda9beb1518a
lucas <lucasaita4000@gmail.com>
2025-09-11 22:22:04 -0300

    feat(infra): cria os DTOS de response e request para usarmos no contoller

---
+++ src/main/java/dev/lucas/EventApi/Infra/dto/EventRequest.java
@@ -0,0 +1,19 @@
+package dev.lucas.EventApi.Infra.dto;
+
+import dev.lucas.EventApi.Core.Enums.TypeEvent;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
+@Builder
+public record EventRequest( Long id,
+                            String name,
+                            String description,
+                            LocalDateTime dateStart,
+                            LocalDateTime dateFinish,
+                            String identificator,
+                            String localEvent,
+                            String organization,
+                            String capacity,
+                            TypeEvent type) {
+}
