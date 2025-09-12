7fd9eaf4f7809d3366bfa4e4d71cc657e7d16614
lucas <lucasaita4000@gmail.com>
2025-09-12 04:11:16 -0300

    feat(core): CreateEventeCaseImpl recebe a injecao do Gateway para receber a abstracao do método de criar um event

--- src/main/java/dev/lucas/EventApi/Core/usecases/CreateEventCaseImpl.java
+++ src/main/java/dev/lucas/EventApi/Core/usecases/CreateEventCaseImpl.java
@@ -1,12 +1,19 @@
 package dev.lucas.EventApi.Core.usecases;
 
 import dev.lucas.EventApi.Core.entities.Event;
+import dev.lucas.EventApi.Core.gateway.EventGateway;
 
 
 public class CreateEventCaseImpl implements CreateEventCase{
 
+    private final EventGateway eventGateway;
+
+    public CreateEventCaseImpl(EventGateway eventGateway) {
+        this.eventGateway = eventGateway;
+    }
+
     @Override
     public Event execute(Event event) {
-        return new Event(event.id(), event.name(), event.description(), event.start(), event.finish(), event.indentificator(), event.localEvent(), event.organization(), event.capacity(), event.type());
+        return eventGateway.createEvent(event);
     }
 }
