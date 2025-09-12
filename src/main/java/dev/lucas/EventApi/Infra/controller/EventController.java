b376797c25f49ed99a71f24c5e48d947bc78ba8c
lucas <lucasaita4000@gmail.com>
2025-09-11 22:26:04 -0300

    feat(infra): cria nossa primeira rota no controller para criar eventos

---
+++ src/main/java/dev/lucas/EventApi/Infra/controller/EventController.java
@@ -0,0 +1,29 @@
+package dev.lucas.EventApi.Infra.controller;
+
+import dev.lucas.EventApi.Core.entities.Event;
+import dev.lucas.EventApi.Core.usecases.CreateEventCase;
+import dev.lucas.EventApi.Infra.dto.EventRequest;
+import dev.lucas.EventApi.Infra.dto.EventResponse;
+import dev.lucas.EventApi.Infra.mapper.EventMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping ("/api/v1")
+public class EventController {
+
+    private CreateEventCase createEventCase;
+
+    @PostMapping("/create")
+    public EventResponse newEvent (@RequestBody EventRequest eventRequest){
+        Event event = EventMapper.toEvent(eventRequest);
+        Event createEvent = createEventCase.execute(event);
+        return EventMapper.toResponse(createEvent);
+
+    }
+
+}
