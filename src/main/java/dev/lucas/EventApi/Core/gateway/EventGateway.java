a6528a832b70a45937d0d99b151c115d6f689784
lucas <lucasaita4000@gmail.com>
2025-09-15 02:47:02 -0300

    feat(core): Adiciona nossa lista de eventos no EventGateway para o infra conectar e receber a inversao de dependencia

--- src/main/java/dev/lucas/EventApi/Core/gateway/EventGateway.java
+++ src/main/java/dev/lucas/EventApi/Core/gateway/EventGateway.java
@@ -2,7 +2,11 @@ package dev.lucas.EventApi.Core.gateway;
 
 import dev.lucas.EventApi.Core.entities.Event;
 
+import java.util.List;
+
 public interface EventGateway {
 
     public Event createEvent (Event event);
+
+    public List<Event> listEvent ();
 }
