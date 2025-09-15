c1b6ec5c2add99febf68149144a7f2c6165616fb
lucas <lucasaita4000@gmail.com>
2025-09-15 02:49:53 -0300

    feat(infra): recebe o método da interface EventGateway, onde realiza a sobreescrita do mesmo, possibilitando assim o Jpa realizar o método de .findAll

--- src/main/java/dev/lucas/EventApi/Infra/gateway/EventRepositoryGateway.java
+++ src/main/java/dev/lucas/EventApi/Infra/gateway/EventRepositoryGateway.java
@@ -3,10 +3,13 @@ package dev.lucas.EventApi.Infra.gateway;
 import dev.lucas.EventApi.Core.entities.Event;
 import dev.lucas.EventApi.Core.gateway.EventGateway;
 import dev.lucas.EventApi.Infra.mapper.EventEntityMapper;
+import dev.lucas.EventApi.Infra.mapper.EventMapper;
 import dev.lucas.EventApi.Infra.persistence.entities.EventsEntity;
 import dev.lucas.EventApi.Infra.persistence.repository.EventRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -19,5 +22,13 @@ public class EventRepositoryGateway implements EventGateway {
         EventsEntity entity = EventEntityMapper.toEntity(event);
         EventsEntity newEvent = eventRepository.save(entity);
         return EventEntityMapper.toDomain(newEvent);
+    }
+
+    @Override
+    public List<Event> listEvent() {
+        List<EventsEntity> eventsEntities = eventRepository.findAll();
+        return eventsEntities.stream()
+                .map(eventsEntity -> EventEntityMapper.toDomain(eventsEntity))
+                .toList();
     }
 }
