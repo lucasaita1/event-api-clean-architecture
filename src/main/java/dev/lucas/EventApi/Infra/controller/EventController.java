87f580fddccc2bc7401507670e3e07b57f0eecbf
lucas <lucasaita4000@gmail.com>
2025-09-15 03:45:54 -0300

    feat(infra): adiciona ResponseEntity no controller para dar respostas mais amigáveis e personalizadas para as requisicoes do usuário

--- src/main/java/dev/lucas/EventApi/Infra/controller/EventController.java
+++ src/main/java/dev/lucas/EventApi/Infra/controller/EventController.java
@@ -7,9 +7,14 @@ import dev.lucas.EventApi.Infra.dto.EventRequest;
 import dev.lucas.EventApi.Infra.dto.EventResponse;
 import dev.lucas.EventApi.Infra.mapper.EventMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -21,20 +26,23 @@ public class EventController {
     private final ListEventCase listEventCase;
 
     @PostMapping("/create")
-    public EventResponse newEvent (@RequestBody EventRequest eventRequest){
+    public ResponseEntity<Map<String, Object>> newEvent (@RequestBody EventRequest eventRequest){
         Event event = EventMapper.toEvent(eventRequest);
         Event createEvent = createEventCase.execute(event);
-        return EventMapper.toResponse(createEvent);
-
+        EventMapper.toResponse(createEvent);
+        Map<String, Object> response = new HashMap<>();
+        response.put("Message: ", " event registered successfully!");
+        response.put("Event: ", createEvent);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @GetMapping
-    public List<EventResponse> getAllEvents (){
+    public ResponseEntity<List<EventResponse>> getAllEvents (){
         List<Event> eventList = listEventCase.execute();
         List<EventResponse> responses = eventList.stream()
                 .map(event -> EventMapper.toResponse(event))
                 .collect(Collectors.toList());
-        return responses;
+        return ResponseEntity.ok(responses);
     }
 
 }
