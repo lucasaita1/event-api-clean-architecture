bcbf32e39e07738754b44e3d0bd3d378cf19a6f4
lucas <lucasaita4000@gmail.com>
2025-09-16 04:06:48 -0300

    feat(infra): cria método findByIdentificator para usarmos como filtro de buscas para endpoints e para ser usado no método de criar eventos, onde o mesmo valida se o identificador fornecido é único ou já existente. Caso seja existente, lançará uma exception de IllegalArgumentIdentifier e bloqueará a criacao de um novo evento. Caso nao exista, deixará o método de criar evento seguir sua trajetória

--- src/main/java/dev/lucas/EventApi/Infra/persistence/repository/EventRepository.java
+++ src/main/java/dev/lucas/EventApi/Infra/persistence/repository/EventRepository.java
@@ -3,5 +3,9 @@ package dev.lucas.EventApi.Infra.persistence.repository;
 import dev.lucas.EventApi.Infra.persistence.entities.EventsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EventRepository extends JpaRepository<EventsEntity, Long> {
+    
+    public Optional<EventsEntity> findByIdentificator (String identificator);
 }
