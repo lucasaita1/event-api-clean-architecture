d909c660c2d777d36f79c263e9ab7a047a3f773a
lucas <lucasaita4000@gmail.com>
2025-09-16 04:10:05 -0300

    feat(infra): adiciona a validacao de identificador e a exception no método de criar eventos

--- src/main/java/dev/lucas/EventApi/Infra/gateway/EventRepositoryGateway.java
+++ src/main/java/dev/lucas/EventApi/Infra/gateway/EventRepositoryGateway.java
@@ -2,14 +2,15 @@ package dev.lucas.EventApi.Infra.gateway;
 
 import dev.lucas.EventApi.Core.entities.Event;
 import dev.lucas.EventApi.Core.gateway.EventGateway;
+import dev.lucas.EventApi.Infra.exceptions.IllegalArgumentIdentifier;
 import dev.lucas.EventApi.Infra.mapper.EventEntityMapper;
-import dev.lucas.EventApi.Infra.mapper.EventMapper;
 import dev.lucas.EventApi.Infra.persistence.entities.EventsEntity;
 import dev.lucas.EventApi.Infra.persistence.repository.EventRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
@@ -19,6 +20,13 @@ public class EventRepositoryGateway implements EventGateway {
 
     @Override
     public Event createEvent(Event event) {
+        EventsEntity verifier = EventEntityMapper.toEntity(event);
+        Optional<EventsEntity> optionalEvents = eventRepository.findByIdentificator(verifier.getIdentificator());
+
+        if (optionalEvents.isPresent()){
+            throw new IllegalArgumentIdentifier("Event already exists with this identifier!");
+        }
+
         EventsEntity entity = EventEntityMapper.toEntity(event);
         EventsEntity newEvent = eventRepository.save(entity);
         return EventEntityMapper.toDomain(newEvent);
@@ -30,5 +38,11 @@ public class EventRepositoryGateway implements EventGateway {
         return eventsEntities.stream()
                 .map(eventsEntity -> EventEntityMapper.toDomain(eventsEntity))
                 .toList();
+    }
+
+    @Override
+    public Optional<Event> findByIdentificator(String identificator) {
+        Optional<EventsEntity> optionalEntity = eventRepository.findByIdentificator(identificator);
+        return optionalEntity.map(entity -> EventEntityMapper.toDomain(entity));
     }
 }
