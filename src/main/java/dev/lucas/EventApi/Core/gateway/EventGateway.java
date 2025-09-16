116e966ece985986a46f6d6dcf8f7ecbbbd4edb4
lucas <lucasaita4000@gmail.com>
2025-09-16 04:17:52 -0300

    feat(core): recebe o método findByIdentificator para ser injetado no FindEventByIdentificatorUseCaseImpl e também conectar essa implementacao do UseCase no Infra

--- src/main/java/dev/lucas/EventApi/Core/gateway/EventGateway.java
+++ src/main/java/dev/lucas/EventApi/Core/gateway/EventGateway.java
@@ -3,10 +3,13 @@ package dev.lucas.EventApi.Core.gateway;
 import dev.lucas.EventApi.Core.entities.Event;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface EventGateway {
 
     public Event createEvent (Event event);
 
     public List<Event> listEvent ();
+
+    Optional<Event> findByIdentificator(String identificator);
 }
